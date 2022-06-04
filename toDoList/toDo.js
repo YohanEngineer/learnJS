ab05596c08417139dee0f99ffabd2c144a37a574
YohanEngineer <bouali.yohan@yahoo.com>
2022-06-04 15:44:51 +0200

    improve to do list with modify and deletion

--- toDoList/toDo.js
+++ toDoList/toDo.js
@@ -10,23 +10,29 @@ function createRow(nbTask, taskName) {
     nbTask++;
     let index = 'task' + nbTask;
     let isTaskExisting = localStorage.getItem(index) !== null;
-    console.log(isTaskExisting);
     if (!isTaskExisting) {
         let task = taskName;
         localStorage.setItem(index, task);
         localStorage.setItem('nbTask', nbTask);
     }
     let tr = document.createElement('tr');
+    tr.setAttribute('id', 'tr' + nbTask);
     let rowNumber = document.createElement('th');
     rowNumber.setAttribute('scope', 'row');
     rowNumber.textContent = nbTask;
     let col1 = document.createElement('td');
     let done = createCheckBox(nbTask);
     col1.appendChild(done);
+    let check = localStorage.getItem('check'+nbTask) !== null;
     let col2 = document.createElement('td');
     col2.setAttribute('id', 'task' + nbTask);
     col2.textContent = taskName;
-    col2.setAttribute('style', '');
+    if(check) {
+        col2.setAttribute('style', 'text-decoration : line-through;');
+        done.setAttribute('checked', '');
+    } else {
+        col2.setAttribute('style', '');
+    }
     let col3 = document.createElement('td');
     let manage = createManageButton(nbTask);
     col3.appendChild(manage);
@@ -36,6 +42,7 @@ function createRow(nbTask, taskName) {
     tr.appendChild(col3);
     let tbody = document.getElementById('tbody');
     tbody.appendChild(tr);
+
     return nbTask;
 }
 
@@ -45,8 +52,41 @@ function createManageButton(index) {
     manage.setAttribute('class', 'btn btn-sm btn-light');
     manage.setAttribute('id', 'manage' + index);
     manage.textContent = 'Manage';
-    manage.addEventListener('click', () => {
-        
+    manage.addEventListener('click', async () => {
+        const inputValue = '';
+        const { value: nextValue } = await Swal.fire({
+            title: 'Manage task',
+            text: 'What do you want to do ?',
+            icon: 'info',
+            input: 'text',
+            inputLabel: 'Your task',
+            inputValue: inputValue,
+            confirmButtonText: 'Modify',
+            showDenyButton: true,
+            denyButtonText: 'Delete',
+            allowOutsideClick: false,
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'You need to write something!'
+                }
+                if(value) {
+                    console.log(value);
+                    localStorage.setItem('task' + index, value);
+                    let task = document.getElementById('task' + index);
+                    task.textContent = value;
+                }
+            }
+        }).then((result) => {
+            if (result.isDenied) {
+                let tbody = document.getElementById('tbody');
+                let tr = document.getElementById('tr' + index);
+                tbody.removeChild(tr);
+                localStorage.removeItem('task' + index);
+                localStorage.removeItem('check' + index);
+                let oldNbTask = localStorage.getItem('nbTask');
+                localStorage.setItem('nbTask', oldNbTask--);
+            }
+        });
     })
     return manage;
 }
@@ -59,17 +99,15 @@ function createCheckBox(index) {
     done.setAttribute('id', 'checkbox' + index);
     done.addEventListener('input', () => {
         let textTask = document.getElementById('task' + index);
-        if(textTask.getAttribute('style').includes('line')) {
+        if (textTask.getAttribute('style').includes('line')) {
             textTask.setAttribute('style', 'text-decoration : none;');
-            localStorage.removeItem('check'+index);
-            localStorage.setItem('check'+index, 'false');
-            console.log(textTask.getAttribute('style'));
+            localStorage.removeItem('check' + index);
+            localStorage.setItem('check' + index, 'false');
         } else {
             textTask.setAttribute('style', 'text-decoration : line-through;');
-            localStorage.removeItem('check'+index);
-            localStorage.setItem('check'+index, 'true');
+            localStorage.removeItem('check' + index);
+            localStorage.setItem('check' + index, 'true');
         }
-     
     });
     return done;
 }
@@ -79,11 +117,13 @@ function loadTasks() {
     if (nbTask < 1) {
         return;
     }
-    for(let i = 0; i < nbTask; i++) {
-        let taskText = localStorage.getItem('task'+(i+1));
-        numberOfTask = createRow(i,taskText);
+    let tasks = [];
+    for (let i = 0; i < nbTask; i++) {
+        let taskText = localStorage.getItem('task' + (i + 1));
+        numberOfTask = createRow(i, taskText);
     }
 }
+
 
 submit.addEventListener('click', () => {
     numberOfTask = createRow(numberOfTask, retrieveNewTaskValue());
